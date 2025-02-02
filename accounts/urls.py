aba789fc19165213c5a080433f160c511ca5fc27
Esler Escobar <escobarcarpio@gmail.com>
2025-02-01 20:41:35 -0600

    primer commit

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    # Vista de login usando la vista genérica de Django
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    # Vista de logout (redirige a la página de login al cerrar sesión)
+    path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
+]
