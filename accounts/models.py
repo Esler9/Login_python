e8ec5a977677ae1e6b9744b98ed6f4dd6c88de29
Esler Escobar <escobarcarpio@gmail.com>
2025-02-01 21:44:21 -0600

    asdasd

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
-
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True, max_length=255)
+    # Campos adicionales si es necesario
     phone_number = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
